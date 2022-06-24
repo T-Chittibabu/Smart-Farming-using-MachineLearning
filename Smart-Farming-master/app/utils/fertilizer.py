446285b1652c04db099c992cc4736e7dad72d4bd
Chittibabu Terala <102535438+T-Chittibabu@users.noreply.github.com>
2022-06-24 11:08:29 +0530

    first commit

---
+++ Smart-Farming-master/app/utils/fertilizer.py
@@ -0,0 +1,20 @@
+fertilizer_dic = {
+        '0': """ Cluster 0:
+        <br/> Please consider the following suggestions:
+        <br/> Your soil contains high amount of nitrogen and average amount of phosphorous and pottassium. The soil is acidic and moisture content is average.
+        """,
+
+        '1': """Cluster 1: 
+        <br/> Please consider the following suggestions:
+        <br/> Your soil contains excess of pottassium and phosphourous while it highly lacks nitrogen.The soil is highly acidic and moisture content is above normal.
+        """,
+
+        '2': """Cluster 2: 
+        <br/> Please consider the following suggestions:
+        <br/> Your soil contains has a deficiency of nitrogen, phosphorous and pottassium. The soil is highly acidic and moisture content is very low.
+        """,
+
+        '3': """Cluster 3: <br/> Please consider the following suggestions:
+        <br/> The nitrogen content is slightly low and average amount of phosphorous and pottassium. The soil is acidic and moisture content is good.
+        """
+    }
